f6a7d24e79c1664b1973d52fbde3aa8507595518
Benjamin Arnaudet <54038112+benjarn374@users.noreply.github.com>
2024-03-22 15:45:57 +0100

    Update events.php

--- private/views/events.php
+++ private/views/events.php
@@ -29,29 +29,27 @@
     });
 </script>
 <?php
-
 $sql = 'SELECT * FROM evenements';
 $stmt = $pdo->query($sql);
 $evenements = $stmt->fetchAll();
-
-if (empty($evenements)) {
-        echo "<p>Aucun événement à afficher.</p>";
-} else {
-        echo '<div class="flex justify-center items-center h-screen mt-8">
-            <div class="w-3/4">
-                <h1 class="text-3xl font-bold mb-4">Événements</h1>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">';
-
-        foreach ($evenements as $evenement) {
-                echo '
-                <div class="bg-white shadow-md rounded p-4">
-                <img src="public/images/DALL·E-2024-03-14-14.39.jpg" alt="Image d\'accueil" class="w-full h-96 object-cover rounded-md shadow-md mb-8">
-                <h2 class="text-xl font-bold mb-2">' . htmlspecialchars($evenement['titre']) . '</h2>
-                <p class="text-sm mb-2">' . htmlspecialchars($evenement['description']) . '</p>
-                <p class="text-sm mb-2">Le ' . FrontUtils::FormatDate($evenement['date']) . '</p>
-                <a href="#" class="voir-evenement bg-blue-500 text-white rounded p-2 hover:bg-blue-600" data-evenement-id="' . htmlspecialchars($evenement['id']) . '">Voir l\'événement</a>
-                </div>';
-        }
-
-        echo '</div></div></div>';
-}
+?>
+<?php if (empty($evenements)) : ?>
+    <p>Aucun événement à afficher.</p>
+<?php else : ?>
+    <div class="flex justify-center items-center h-screen mt-8">
+        <div class="w-3/4">
+            <h1 class="text-3xl font-bold mb-4">Événements</h1>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">';
+                <?php foreach ($evenements as $evenement) : ?>
+                    <div class="bg-white shadow-md rounded p-4">
+                        <img src="public/images/DALL·E-2024-03-14-14.39.jpg" alt="Image d\'accueil" class="w-full h-96 object-cover rounded-md shadow-md mb-8">
+                        <h2 class="text-xl font-bold mb-2"><?= htmlspecialchars($evenement['titre']) ?></h2>
+                        <p class="text-sm mb-2"><?= htmlspecialchars($evenement['description']) ?></p>
+                        <p class="text-sm mb-2">Le <?= FrontUtils::FormatDate($evenement['date']) ?></p>
+                        <a href="#" class="voir-evenement bg-blue-500 text-white rounded p-2 hover:bg-blue-600" data-evenement-id="<?= htmlspecialchars($evenement['id']) ?>">Voir l\'événement</a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
