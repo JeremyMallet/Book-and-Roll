f9ff42330da12bc2f9f8e5d26487417b4ea7c334
JeremyMallet <jeremy.mallet@oclock.school>
2024-03-23 14:00:41 +0100

    Ajouter une favicon

--- private/view.php
+++ private/view.php
@@ -10,6 +10,7 @@
     <script src="public/js/Modal.js"></script>
     <script src="public/js/Toast.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/x-icon" href="public/images/DALL·E-2024-03-14-15.06.ico">
 </head>
 
 <body class="mt-20">
