25a2b58a54d7f89819caa504de7078ab8dba4569
benjarn374 <54038112+benjarn374@users.noreply.github.com>
2024-03-24 07:58:47 +0100

    merging sql + db.php
    create a installation folder with a single file of installation.
    removing db connexion from config file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Book-and-Roll
+
+Nous utilisons xampp sous windows pour le développement de Book-and-Roll : https://www.apachefriends.org/fr/index.html
+
+## installation
+Cloner le repository dans le dossier c:\\xampp\htdocs
+Créez une database nommée 'bookandroll' dans Xampp
+Chargez la BDD dans PHP My Admin
+Ouvrez https://localhost/Book-and-Roll/
--- index.php
+++ index.php
@@ -1,34 +1,20 @@
 <?php
-    require 'config.php';
-    require 'private/utils/FrontUtils.php';
-    require 'private/class/evenement.php';
+require './config.php'; // chargement de la configuration
 
-    // controlleur de post
-    if (!empty($_GET['post'])) {
-        include("./private/post/{$_GET['post']}.php");
-        die;
-    }
+require ROOT . 'private/db.php';                // connexion a la base de données
+require ROOT . 'private/utils/FrontUtils.php';  // utilitaires de Front
+require ROOT . 'private/class/evenement.php';   // class Evenement
 
-    // controlleur de vues index.php?view=home ou index.php?view=events
-    if(empty($_GET['view'])) {
-        $view = 'home';
-    }else{
-        $view = $_GET['view'];
-    }
-    include('./private/view.php');
+// controlleur de post
+if (!empty($_GET['post'])) {
+    include("./private/post/{$_GET['post']}.php");
+    die;
+}
 
-    /*
-    $sql = 'SELECT id, titre, date, description FROM evenements';
-    $stmt = $pdo->query($sql);
-
-    while ($row = $stmt->fetch()) {
-        echo $row['titre'] . "<br>\n";
-    }
-    
-
-
-    */
-
-
-
-
+// controlleur de vues index.php?view=home ou index.php?view=events
+if (empty($_GET['view'])) {
+    $view = 'home';
+} else {
+    $view = $_GET['view'];
+}
+include('./private/view.php');
